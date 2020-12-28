cab7e0552ce94cfdb0d4d6bdc9ecae2ffb381e7a
ivanliakh <69403584+ivanliakh@users.noreply.github.com>
2020-12-27 22:43:10 -0500

    My first commit

---
+++ join.py
@@ -0,0 +1,12 @@
+myList = ["a", "b", "c", "d"]
+letters = "abcdefghijklmnopqrstuvwxyz"
+numbers = "123456789"
+
+newString = ", ".join(myList)
+print(newString)
+
+newString1 = ", ".join(letters)
+print(newString1)
+
+newString2 = " mississippi ".join(numbers)
+print(newString2)
